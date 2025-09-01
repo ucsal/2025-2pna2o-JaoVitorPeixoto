f1fd16f0cd7f0d4fd0381d870d56d5b844ba3022
João Peixoto - Personal <joaottvitor04@gmail.com>
2025-08-31 22:04:58 -0300

    refactor: Refazendo o DiscountCalculator para rodar com a interface e refazendo a Main e test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 import br.com.mariojp.solid.ocp.*;
+import br.com.mariojp.solid.ocp.policies.*;
 
 public class Main {
 	public static void main(String[] args) {
 		DiscountCalculator calc = new DiscountCalculator();
-		System.out.println("REGULAR 100 -> " + calc.apply(100, CustomerType.REGULAR));
-		System.out.println("PREMIUM 100 -> " + calc.apply(100, CustomerType.PREMIUM));
+		System.out.println("REGULAR 100 -> " + calc.apply(100, new RegularPolicy()));
+		System.out.println("PREMIUM 100 -> " + calc.apply(100, new PremiumPolicy()));
 		// PARTNER deveria ter 12%
-		System.out.println("PARTNER 100 -> " + calc.apply(100, CustomerType.PARTNER));
+		System.out.println("PARTNER 100 -> " + calc.apply(100, new PartnerPolicy()));
 	}
 }
--- src/test/java/br/com/mariojp/solid/ocp/DiscountCalculatorTest.java
+++ src/test/java/br/com/mariojp/solid/ocp/DiscountCalculatorTest.java
@@ -1,13 +1,16 @@
 package br.com.mariojp.solid.ocp;
 
 import org.junit.jupiter.api.Test;
+
+import br.com.mariojp.solid.ocp.policies.PartnerPolicy;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DiscountCalculatorTest {
     @Test
     void partner_gets_12_percent() {
         var calc = new DiscountCalculator();
-        assertEquals(88.0, calc.apply(100.0, CustomerType.PARTNER), 0.0001,
+        assertEquals(88.0, calc.apply(100.0, new PartnerPolicy()), 0.0001,
             "PARTNER deveria ter 12% de desconto");
     }
 }
